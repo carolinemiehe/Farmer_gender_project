97cd2ad1052fb28cef0bdca04cdcb3a84d60586c
Emma Prati <emmaprati27gmail.com>
2025-11-11 16:49:03 +0100

    Updated Descriptives.R and Emmas_paper.lyx

--- papers/adoption_gaps_paper/Descriptives.R
+++ papers/adoption_gaps_paper/Descriptives.R
@@ -170,18 +170,6 @@ baseline_farmers$education_head_num <- as.numeric(dplyr::recode(
 summary(baseline_farmers$education_head_num)
 
 
-
-
-#shall I delete the category 'other'?
-
-## Q24: Member of farmer group / cooperative
-baseline_farmers$farmer_group_member <- baseline_farmers$Check2.check.maize.q24
-baseline_farmers$farmer_group_member[baseline_farmers$farmer_group_member %in% c("98","999","n/a","NA","")] <- NA
-
-baseline_farmers$farmer_group_member <- ifelse(baseline_farmers$farmer_group_member == "Yes", 1,
-                                               ifelse(baseline_farmers$farmer_group_member == "No", 0, NA))
-table(baseline_farmers$farmer_group_member)
-
 #LABOUR MARKET IMPERFECTIONS - TIME POVERTY AND CARE RESPONSABILITY
 #Q18 -household size (number of members)
 baseline_farmers$household_size <- baseline_farmers$Check2.check.maize.q18
@@ -226,13 +214,15 @@ table(baseline_farmers$respondent_is_hh)
 
 
 #Q14(Age of household head)
-baseline_farmers$Check2.check.maize.q14 <- as.numeric(as.character(baseline_farmers$Check2.check.maize.q14))
-baseline_farmers$Check2.check.maize.q14[baseline_farmers$Check2.check.maize.q14 == 999] <- NA
-baseline_farmers$Check2.check.maize.q14[baseline_farmers$Check2.check.maize.q14 < 15 |baseline_farmers$Check2.check.maize.q14 > 99] <- NA
-table(baseline_farmers$Check2.check.maize.q14)
-
-summary(baseline_farmers$Check2.check.maize.q14)
+baseline_farmers$Check2.check.maize.q14 <- as.character(baseline_farmers$Check2.check.maize.q14)
+baseline_farmers$Check2.check.maize.q14[
+  baseline_farmers$Check2.check.maize.q14 %in% c("999", "9999", "n/a", "NA", "", " ")] <- NA
+baseline_farmers$Check2.check.maize.q14 <- as.numeric(baseline_farmers$Check2.check.maize.q14)
+baseline_farmers$Check2.check.maize.q14[
+  baseline_farmers$Check2.check.maize.q14 < 15 | baseline_farmers$Check2.check.maize.q14 > 99] <- NA
 baseline_farmers$hh_age <- baseline_farmers$Check2.check.maize.q14
+summary(baseline_farmers$hh_age)
+
 
 #Q15(Gender of household head)
 baseline_farmers$Check2.check.maize.q15 <- as.character(baseline_farmers$Check2.check.maize.q15)
@@ -279,13 +269,23 @@ baseline_farmers$bags_sold <- as.numeric(as.character(baseline_farmers$bags_sold
 summary(baseline_farmers$bags_sold)
 table(baseline_farmers$bags_sold)
 
-#Q55 - 
+#Q55 - bags price
 baseline_farmers$bag_price <- baseline_farmers$Check2.check.maize.q55
 baseline_farmers$bag_price[baseline_farmers$bag_price %in% c("999","n/a","NA","")] <- NA
 baseline_farmers$bag_price <- as.numeric(as.character(baseline_farmers$bag_price))
 summary(baseline_farmers$bag_price)
 baseline_farmers$bag_price <- baseline_farmers$bag_price / 1000
 table(baseline_farmers$bag_price, useNA = "ifany")
+#put 0 for the ones that did not sell anything
+baseline_farmers <- baseline_farmers %>%
+  mutate(
+    bags_sold = ifelse(maize_sold == 0 & is.na(bags_sold), 0, bags_sold),
+    bag_price = ifelse(maize_sold == 0 & is.na(bag_price), 0, bag_price),
+    maize_income = ifelse(is.na(bags_sold) | is.na(bag_price),
+                          0, bags_sold * bag_price)
+  )
+
+
 
 #OUTPUT VARIABLES 
 baseline_farmers$Check2.check.maize.q50[baseline_farmers$Check2.check.maize.q50 %in% c(999, "999", "n/a", "NA", "", " ")] <- NA
@@ -297,17 +297,8 @@ baseline_farmers$yield_inkg <- baseline_farmers$Check2.check.maize.q50 * baselin
 
 aggregate(yield_inkg ~ hh_gender_num, data = baseline_farmers, mean, na.rm = TRUE)
         
-# Quante osservazioni valide per yield e area?
-sum(!is.na(baseline_farmers$Check2.check.maize.q50))
-sum(!is.na(baseline_farmers$Check2.check.maize.q51))
-sum(!is.na(baseline_farmers$yield_inkg))
 
-# Percentuale di dati mancanti per genere
 table(is.na(baseline_farmers$yield_inkg), baseline_farmers$hh_gender_num)
-
-# Quante righe sono perse nella divisione per area
-sum(!is.na(baseline_farmers$r_maize_plot_area))
-sum(!is.na(baseline_farmers$yield_per_acre))
 
 
 #yield per acre (PRODUCTIVITY)
@@ -336,12 +327,21 @@ summary(baseline_farmers$yield_per_acre_ihs)
 
 
 #MAIZE INCOME (bags sold × bag price)
-baseline_farmers$maize_income <- baseline_farmers$bags_sold * baseline_farmers$bag_price
-baseline_farmers$maize_income[baseline_farmers$maize_income <= 0] <- NA
+baseline_farmers$maize_income <- with(baseline_farmers,
+                                      ifelse(is.na(bags_sold) | is.na(bag_price), 0, bags_sold * bag_price)
+)
+table(is.na(baseline_farmers$maize_income), baseline_farmers$hh_gender_num)
+summary(baseline_farmers$maize_income)
+
+#with this definition of income also people that did not sell anything are included
 table(baseline_farmers$maize_income)
 summary(baseline_farmers$maize_income)
 baseline_farmers$maize_income[baseline_farmers$maize_income %in% c("999","n/a","NA","", 99900)] <- NA
 aggregate(maize_income ~ hh_gender_num, data = baseline_farmers, mean, na.rm = TRUE)
+
+baseline_farmers$maize_income_ihs <- ihs(baseline_farmers$maize_income)
+summary(baseline_farmers$maize_income_ihs)
+table(baseline_farmers$maize_income_ihs)
 #DIVIDED per gender Q15
 # hh_gender_num <- trimws(tolower(as.character(baseline_farmers$Check2.check.maize.q15)))  # rimuove spazi e mette in minuscolo
 # 
@@ -367,20 +367,20 @@ variables <- c(
   "respondent_is_hh", #The respondent is household head (1 = Yes) 
   "hh_marital_status_num", #Household head is married (1 = Yes)
   "quality_seed_used", #The respondent used quality seeds (1 = Yes)
-  "maize_sold", #The household sold maize (1 = Yes)
   "r_maize_plot_area",#Area of the randomly selected plot in acres
   "seed_total_cost_ugx", #Total cost of seed used for the randomly selected plot in UGX
   "dap_npk_applied", #DAP/NPK applied in the randomly selected plot 
   "urea_applied",#Urea applied in the randomly selected plot 
   "chemicals_applied", #Pesticides, herbicides or fungicides applied in the randomly selected plot
-   "bags_sold", #Number of maize bags sold from the randomly selected plot
+  "maize_sold", #The household sold maize from the randomly selected plot(1 = Yes)
+  "bags_sold", #Number of maize bags sold from the randomly selected plot
   "bag_price", #Bag price in thousand UGX
-  "maize_income",
   "yield_inkg",
   "yield_per_acre",
-  "yield_per_acre_ihs"
+  "yield_per_acre_ihs",
+  "maize_income",
+  "maize_income_ihs"
 )
-
 
 
 # MALE
@@ -432,23 +432,229 @@ colSums(is.na(baseline_farmers[, variables])) #to check NAs for each variable
 
 
 #ANALYSIS
-#hist(baseline_farmers$yield_per_acre,
-     #main = "Distribution of yield_per_acre",
-     #xlab = "Yield per acre",
-     #col = "lightblue",
-     #breaks = 30)
+output_vars <- c("yield_per_acre", "yield_per_acre_ihs", "maize_income")
+
+for (var in output_vars) {
+  cat("\n---", var, "---\n")
+  print(t.test(baseline_farmers[[var]] ~ baseline_farmers$hh_gender_num, var.equal = FALSE))
+}
+
+#more complex formula to do a table
+t_test_results <- lapply(output_vars, function(var) {
+  male <- baseline_farmers[[var]][baseline_farmers$hh_gender_num == 1]
+  female <- baseline_farmers[[var]][baseline_farmers$hh_gender_num == 0]
+  ttest <- t.test(male, female, var.equal = FALSE)
+  
+  data.frame(
+    Variable = var,
+    Mean_Male = mean(male, na.rm = TRUE),
+    Mean_Female = mean(female, na.rm = TRUE),
+    Diff = mean(male, na.rm = TRUE) - mean(female, na.rm = TRUE),
+    p_value = ttest$p.value
+  )
+})
+
+t_test_df <- do.call(rbind, t_test_results)
+print(t_test_df)
+
+#Analysis OLS REGRESSIONS
+library (car)
+library(lmtest)
+
+#(respondent_is_hh excluded for collinearity)
+#yield per acre (no ihs) gender is never significant 
+lm1 <- lm(yield_per_acre ~ hh_gender_num, data = baseline_farmers)
+summary(lm1)
+
+lm1_semifull <- lm(yield_per_acre ~ hh_gender_num + education_head_num + household_size +
+                     hh_age + quality_seed_used + dap_npk_applied + urea_applied +
+                     chemicals_applied + r_maize_plot_area, data = baseline_farmers)
+summary(lm1_semifull)
+
+lm1_full <- lm(yield_per_acre ~ hh_gender_num + education_head_num + household_size +
+                 hh_age + hh_marital_status_num + relationship_hh_num +
+                 quality_seed_used + dap_npk_applied + urea_applied + chemicals_applied + distance_agroshops + num_shops + bags_sold + bag_price +
+                 maize_income, data = baseline_farmers)
+summary(lm1_full)
 
 
 
+#lm2 IHS TRANSFORMATION OF YIELD PER ACRE - gender is significant until semifull
+lm2 <- lm(yield_per_acre_ihs ~ hh_gender_num, data = baseline_farmers)
+summary(lm2)
+#Semifull
+lm2_semifull <- lm(yield_per_acre_ihs ~ hh_gender_num + education_head_num + household_size +
+                     hh_age + quality_seed_used + dap_npk_applied + urea_applied +
+                     chemicals_applied + r_maize_plot_area, data = baseline_farmers)
+summary(lm2_semifull)
+resettest(lm2_semifull)
+crPlots(lm2_semifull)
+lm2_semifull_fix <- lm(yield_per_acre_ihs ~ 
+                         hh_gender_num + education_head_num + household_size +
+                         hh_age + I(hh_age^2) +
+                         quality_seed_used + dap_npk_applied +
+                         urea_applied + I(urea_applied^2) +
+                         chemicals_applied + log1p(r_maize_plot_area),
+                       data = baseline_farmers)
+summary(lm2_semifull_fix)
+resettest(lm2_semifull_fix)
+
+#full
+lm2_full <- lm(yield_per_acre_ihs ~ hh_gender_num + education_head_num + household_size +
+                 hh_age + hh_marital_status_num + relationship_hh_num +
+                 quality_seed_used + dap_npk_applied + urea_applied + chemicals_applied + distance_agroshops + num_shops + bags_sold , data = baseline_farmers)
+summary(lm2_full)
+resettest(lm2_full)
+crPlots(lm2_full)
+
+lm2_full_fix <- lm(yield_per_acre_ihs ~ 
+                     hh_gender_num + education_head_num + household_size +
+                     hh_age + I(hh_age^2) +
+                     hh_marital_status_num + relationship_hh_num +
+                     quality_seed_used + dap_npk_applied +
+                     urea_applied + I(urea_applied^2) +
+                     chemicals_applied +
+                     log1p(distance_agroshops) + num_shops + log1p(bags_sold),
+                   data = baseline_farmers)
+resettest(lm2_full_fix)
 
 
 
+#lm3 Maize-income (maybe esclude seed_total_cost_ugx, maize sold and bags sold(plots_maize)) - gender is significant just when there are no control variables
+
+lm3 <- lm(maize_income ~ hh_gender_num, data = baseline_farmers)
+summary(lm3)
+
+lm3_semifull <- lm(maize_income ~ hh_gender_num + r_maize_plot_area +
+                     education_head_num + household_size + hh_age + 
+                     quality_seed_used + dap_npk_applied + urea_applied +
+                     chemicals_applied, data = baseline_farmers)
+summary(lm3_semifull)
+
+lm3_full <- lm(maize_income ~ hh_gender_num + r_maize_plot_area +
+                 education_head_num + household_size + hh_age +
+                 hh_marital_status_num + relationship_hh_num +
+                 quality_seed_used + dap_npk_applied + urea_applied + chemicals_applied + distance_agroshops + num_shops,
+               data = baseline_farmers)
+summary(lm3_full)
 
 
+#lm4
+
+lm4 <- lm(maize_income_ihs ~ hh_gender_num, data = baseline_farmers)
+summary(lm4)
+
+lm4_semifull <- lm(maize_income_ihs ~ hh_gender_num + r_maize_plot_area +
+                     education_head_num + household_size + hh_age +
+                     quality_seed_used + dap_npk_applied + urea_applied +
+                     chemicals_applied, data = baseline_farmers)
+summary(lm4_semifull)
+resettest(lm4_semifull)
+crPlots(lm4_semifull)
+
+lm4_full <- lm(maize_income_ihs ~ hh_gender_num + r_maize_plot_area +
+                 education_head_num + household_size + hh_age +
+                 hh_marital_status_num + relationship_hh_num +
+                 quality_seed_used + dap_npk_applied + urea_applied + chemicals_applied + distance_agroshops + num_shops,
+               data = baseline_farmers)
+summary(lm4_full)
+crPlots(lm4_full)
+
+#MULTICOLLINEARITY
+vif(lm4_full)
 
 
+par(mfrow=c(1,2))
+hist(residuals(lm3_semifull), main="Residuals (Raw Income)", col="skyblue")
+hist(residuals(lm4_semifull), main="Residuals (IHS Income)", col="lightgreen")
+
+#RAMSEY TEST
+resettest(lm1_semifull)
+
+resettest (lm2_full)
+resettest(lm3_semifull)
+resettest(lm4_semifull)
+resettest(lm4_full)
 
 
+#to see linearity
+library(car)
+
+crPlots(lm2_semifull)   # per yield_per_acre_ihs
+crPlots(lm4_semifull)  # per maize_income_ihs (semifull)
+crPlots(lm4_full)     #per maize_income_ihs (full)
 
 
+lm2_semifull_fix <- lm(yield_per_acre_ihs ~ hh_gender_num + hh_age + I(hh_age^2) +
+                         log1p(r_maize_plot_area) + I(urea_applied^2) +
+                         household_size + education_head_num + quality_seed_used +
+                         dap_npk_applied + chemicals_applied, data = baseline_farmers)
+resettest(lm2_semifull_fix)
 
+lm4_semifull_fix <- lm(maize_income_ihs ~ hh_gender_num + hh_age +
+                       log1p(r_maize_plot_area) + I(urea_applied^2) +
+                       household_size + education_head_num + quality_seed_used +
+                       dap_npk_applied + chemicals_applied, data = baseline_farmers)
+resettest(lm4_semifull_fix)
+
+#Etheroskedasticity
+library(sandwich)
+library(lmtest)
+bptest(lm4_full)
+bptest(lm4_semifull_fix)
+
+names(baseline_farmers)
+
+coeftest(lm4_semifull, vcov = vcovCL(lm4_semifull, cluster = baseline_farmers$catchID))
+coeftest(lm4_full, vcov = vcovCL(lm4_full, cluster = baseline_farmers$catchID))
+
+
+#some variables to be added
+#Q42Did you apply organic manure to the soil on this ${plot_select_name}  before planting second season of 2020? 
+
+baseline_farmers$organic_manure_applied <- baseline_farmers$Check2.check.maize.q42
+baseline_farmers$organic_manure_applied[baseline_farmers$organic_manure_applied %in% c("999","98","n/a","NA","")] <- NA
+baseline_farmers$organic_manure_applied <- ifelse(baseline_farmers$organic_manure_applied == "Yes", 1,
+                                      ifelse(baseline_farmers$organic_manure_applied == "No", 0, NA))
+baseline_farmers$organic_manure_applied <- as.numeric(as.character(baseline_farmers$organic_manure_applied))
+summary(baseline_farmers$organic_manure_applied)
+
+# Q45. How many times did  you weed this maize garden during the second season on 2020?
+baseline_farmers$weed_times <- baseline_farmers$Check2.check.maize.q45
+baseline_farmers$weed_times[baseline_farmers$weed_times %in% c("999", "n/a", "NA", "")] <- NA
+baseline_farmers$weed_times <- as.numeric(as.character(baseline_farmers$weed_times))
+baseline_farmers$weed_times[baseline_farmers$weed_times < 0 | baseline_farmers$weed_times > 5] <- NA
+
+summary(baseline_farmers$weed_times)
+table(baseline_farmers$weed_times, useNA = "ifany")
+
+#Q49. Did you re-sow where seeds did not germinate on **${plot_select_name}** in the second season (entoigo) of 2020?
+
+baseline_farmers$resow <- baseline_farmers$Check2.check.maize.q49
+baseline_farmers$resow <- tolower(as.character(baseline_farmers$resow))
+baseline_farmers$resow[baseline_farmers$resow %in% c("n/a","na","","99","999")] <- NA
+baseline_farmers$resow <- ifelse(
+  baseline_farmers$resow %in% c("yes","y","true","1"), 1,
+  ifelse(baseline_farmers$resow %in% c("no","n","false","0"), 0, NA)
+)
+baseline_farmers$resow <- as.numeric(baseline_farmers$resow)
+table(baseline_farmers$resow, useNA = "ifany")
+
+
+#Q30. Was this **${plot_select_name}** plot intercropped in the second season (entoigo) of 2020?
+baseline_farmers$intercropped <- baseline_farmers$Check2.check.maize.q30
+baseline_farmers$intercropped <- tolower(as.character(baseline_farmers$intercropped))
+baseline_farmers$intercropped[baseline_farmers$intercropped %in% c("n/a", "na", "", "99", "999")] <- NA
+baseline_farmers$intercropped <- ifelse(
+  baseline_farmers$intercropped %in% c("yes", "y", "true", "1"), 1,
+  ifelse(baseline_farmers$intercropped %in% c("no", "n", "false", "0"), 0, NA))
+baseline_farmers$intercropped <- as.numeric(baseline_farmers$intercropped)
+table(baseline_farmers$intercropped, useNA = "ifany")
+
+## Q24: Member of farmer group / cooperative
+baseline_farmers$farmer_group_member <- baseline_farmers$Check2.check.maize.q24
+baseline_farmers$farmer_group_member[baseline_farmers$farmer_group_member %in% c("98","999","n/a","NA","")] <- NA
+
+baseline_farmers$farmer_group_member <- ifelse(baseline_farmers$farmer_group_member == "Yes", 1,
+                                               ifelse(baseline_farmers$farmer_group_member == "No", 0, NA))
+table(baseline_farmers$farmer_group_member)
